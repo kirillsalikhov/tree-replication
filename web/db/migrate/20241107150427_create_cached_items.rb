b50cf846052064c26aa29d0f11c8a955852c2f76
kirill <kirill.salikhov@gmail.com>
2024-11-08 21:43:03 +0400

    fix: CachedItem.is_deleted set default false

--- web/db/migrate/20241107150427_create_cached_items.rb
+++ web/db/migrate/20241107150427_create_cached_items.rb
@@ -3,7 +3,7 @@ class CreateCachedItems < ActiveRecord::Migration[7.2]
     create_table :cached_items, id: :uuid do |t|
       t.string :value
       t.uuid :parent_id
-      t.boolean :is_deleted
+      t.boolean :is_deleted, default: false
 
       t.timestamps
     end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_08_093613) do
   create_table "cached_items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "value"
     t.uuid "parent_id"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
