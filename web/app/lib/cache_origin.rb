4a8df80fb5f78b64dd7a51434f870707b21647f4
kirill <kirill.salikhov@gmail.com>
2024-11-09 19:46:24 +0400

    feat: CacheOrigin DbOrigin implemented

--- web/app/lib/cache_origin.rb
+++ web/app/lib/cache_origin.rb
@@ -6,28 +6,21 @@ class CacheOrigin < BaseOrigin
 
   # @param op [Operation::Load]
   def apply_load(op)
-    puts "apply_load"
-    # TODO check exist
     CachedItem.create!(op.data)
   end
 
   # @param op [Operation::Create]
   def apply_create(op)
-    puts "apply_create"
     CachedItem.create!(op.data)
   end
 
   # @param op [Operation::Update]
   def apply_update(op)
-    puts "apply_update"
     CachedItem.update!(op.item_id, value: op.value)
   end
 
   # @param op [Operation::Remove]
   def apply_remove(op)
-    puts "apply_remove"
-    c = CachedItem.find(op.item_id)
-    c.is_deleted = true
-    c.save!
+    CachedItem.update!(op.item_id, is_deleted: true)
   end
 end
--- web/app/lib/db_origin.rb
+++ web/app/lib/db_origin.rb
@@ -1,2 +1,24 @@
 class DbOrigin < BaseOrigin
+  register_op Operation::Create, :apply_create
+  register_op Operation::Update, :apply_update
+  register_op Operation::Remove, :apply_remove
+
+  def apply_create(op)
+    Item.create!(op.data)
+  end
+
+  def apply_update(op)
+    Item.update!(op.item_id, value: op.value)
+  end
+
+  def apply_remove(op)
+    item = Item.find(op.item_id)
+    item.is_deleted = true
+    descendants = item.descendants
+    descendants.each { |d| d.is_deleted = true }
+    Item.transaction do
+      item.save!
+      descendants.each(&:save!)
+    end
+  end
 end
