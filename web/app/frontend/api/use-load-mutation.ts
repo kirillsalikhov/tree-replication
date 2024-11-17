0fd278760336429bf534fb2239617ba62dff2230
kirill <kirill.salikhov@gmail.com>
2024-11-17 16:45:06 +0400

    feat (backend): add error toast when item already loaded to cache

--- web/app/frontend/api/use-load-mutation.ts
+++ web/app/frontend/api/use-load-mutation.ts
@@ -3,15 +3,28 @@ import {
   getCachedItemsQueryKey,
   useLoad,
 } from '@/api/gen/cached-items/cached-items';
+import { useToast } from '@/providers/toast-provider';
 
 export const useLoadMutation = () => {
   const queryClient = useQueryClient();
+  const { showToast } = useToast();
+
   return useLoad({
     mutation: {
-      onSuccess: () => {
-        void queryClient.invalidateQueries({
-          queryKey: getCachedItemsQueryKey(),
-        });
+      onSettled(data) {
+        if (data?.status === 200) {
+          void queryClient.invalidateQueries({
+            queryKey: getCachedItemsQueryKey(),
+          });
+        } else {
+          // @ts-expect-error Orval has problems with fetch and errors (it doesn't check response code)
+          const errorMessage: string = data?.data.message;
+          showToast({
+            severity: 'error',
+            summary: 'Cache',
+            detail: errorMessage,
+          });
+        }
       },
     },
   });
