30447c3ccc4f1b0bc498d065fc6143e93b52000c
kirill <kirill.salikhov@gmail.com>
2024-11-17 16:20:19 +0400

    feat (backend): add rescue_from to Api::ApplicationController
    ActiveRecord::RecordNotFound => not_found
    
    ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed, ActiveInteraction::InvalidInteractionError => validation_error

--- web/app/controllers/api/application_controller.rb
+++ web/app/controllers/api/application_controller.rb
@@ -1,2 +1,38 @@
 class Api::ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+  rescue_from ActiveRecord::RecordInvalid,
+              ActiveModel::StrictValidationFailed,
+    with: :handle_unprocessable_entity
+
+  rescue_from ActiveInteraction::InvalidInteractionError, with: :handle_interaction_error
+
+  protected
+
+  def handle_not_found(e)
+    render_error "not_found",
+                 e.message,
+                 status: :not_found
+  end
+
+  def handle_unprocessable_entity(e)
+    render_error "validation_error",
+                 e.message,
+                 detail: e.try(:record).try(:errors),
+                 status: :unprocessable_entity
+  end
+
+  def handle_interaction_error(e)
+    render_error "validation_error",
+                 e.message,
+                 detail: e.interaction.errors,
+                 status: :unprocessable_entity
+  end
+
+  def render_error(type, message, detail: nil, status: 500)
+    render json:  {
+      type: type,
+      message: message,
+      detail: detail
+    }, status: status
+  end
 end
