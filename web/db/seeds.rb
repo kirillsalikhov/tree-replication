0754310c608176b6cc2d9efdfd6bbf2f49c2d008
kirill <kirill.salikhov@gmail.com>
2024-11-18 16:45:16 +0400

    chore(backend): remove unused code

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -3,26 +3,4 @@ def set_default_preset
   ResetToPreset.run!(name: "4-level")
 end
 
-def create_items
-  return if Item.any?
-  new_item = _create_item_seq
-
-  root = new_item.()
-  root.children << new_item.()
-  root.children << new_item.()
-    .tap { |child| child.children << new_item.() }
-  root.children << new_item.()
-
-  root.save
-end
-
-def _create_item_seq
-  name = "Node 00"
-
-  -> () do
-    Item.new(value: (name = name.succ))
-  end
-end
-
 set_default_preset
-# create_items
