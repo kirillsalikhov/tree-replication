0dbf719725b0ffe3fd5795e10c1d102e042b6bef
kirill <kirill.salikhov@gmail.com>
2024-11-18 17:37:18 +0400

    fix(backend): add validation for value presence

--- web/app/frontend/api/use-update-mutation.ts
+++ web/app/frontend/api/use-update-mutation.ts
@@ -3,15 +3,28 @@ import {
   getCachedItemsQueryKey,
   useUpdate,
 } from '@/api/gen/cached-items/cached-items';
+import { useToast } from '@/providers/toast-provider';
 
 export const useUpdateMutation = () => {
   const queryClient = useQueryClient();
+  const { showToast } = useToast();
+
   return useUpdate({
     mutation: {
-      onSuccess() {
-        void queryClient.invalidateQueries({
-          queryKey: getCachedItemsQueryKey(),
-        });
+      onSuccess(data) {
+        if (data?.status === 200) {
+          void queryClient.invalidateQueries({
+            queryKey: getCachedItemsQueryKey(),
+          });
+        } else {
+          // @ts-expect-error Orval has problems with fetch and errors (it doesn't check response code)
+          const errorMessage: string = data?.data.message;
+          showToast({
+            severity: 'error',
+            summary: 'Cache',
+            detail: errorMessage,
+          });
+        }
       },
     },
   });
--- web/app/models/operation/update.rb
+++ web/app/models/operation/update.rb
@@ -1,3 +1,4 @@
 class Operation::Update < Operation::Base
   store_accessor :data,  :value
+  validates :value, presence: true
 end
