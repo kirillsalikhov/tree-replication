74f376b0af161f3b22761f8757dfbe2c6152b9d7
kirill <kirill.salikhov@gmail.com>
2024-11-18 13:23:57 +0400

    build: add prod mode to integration
    --mod=p

--- integration/bin/main.sh
+++ integration/bin/main.sh
@@ -3,6 +3,16 @@
 usage="
 Script usage:
   bin/main.sh [--mod=MOD_LETTERS] {up|down|stop|other docker-compose cmd}
+
+  MOD_LETTERS:
+    p - prod mode
+  Examples:
+    main.sh up
+
+    // modify behavior
+    main.sh --mod=p build web // build rails container
+    // then
+    main.sh --mod=p up
 "
 
 cd "$(dirname "$0")"
@@ -11,8 +21,32 @@ if [[ -z $@ ]]; then
     echo "$usage"
 fi
 
+ARGS=()
+mod=''
+for i in "$@"
+do
+    if [[ $i == "--mod="* ]]
+    then
+        mod="${i#*=}"
+        shift
+    else
+        ARGS+=("$i")
+    fi
+done
+echo $mod
+
 base_compose="-f ../compose/base.yml"
 env_file="--env-file ../../web/.env"
 mod_compose=""
+
+for l in $(echo ${mod} | grep -o .)
+do
+    case $l in
+        p)
+            echo "MOD prod env : p"
+            mod_compose+=" -f ../compose/mod/prod.yml"
+        ;;
+    esac
+done
 
 docker compose ${env_file} ${base_compose} ${mod_compose} -p tree_replication "$@"
